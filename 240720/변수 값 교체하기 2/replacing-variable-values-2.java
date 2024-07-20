8c4a24537cf4b7e6f072ff793d19e8beb512be70
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-20 21:11:10 +0900

    Title: 변수 값 교체하기 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240720/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        a = b;
+        System.out.println(a); // 3
+        System.out.println(b); // 3
+    }
+}
