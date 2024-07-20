d2cef080be9400837147247b2b64b9c5a5f28b6a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-20 21:13:29 +0900

    Title: 정수 복사, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240720/정수 복사/copy-integer.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 4;
+        b = a;
+        System.out.println(a + " " + b); // 3 3
+        System.out.println(a * b);       // 9
+    }
+}
