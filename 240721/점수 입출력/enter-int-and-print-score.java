1a39e1bd5f02b5a3b0e7281d69b631f644b03d01
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-21 13:12:39 +0900

    Title: 점수 입출력, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 정수 입력받기
+
+        System.out.printf("Your score is %d point.", n);
+    }
+}
