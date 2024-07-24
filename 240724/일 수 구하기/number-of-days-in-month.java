da7c5deef64d743e8be9fbe7b17460f93506b069
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-24 22:32:48 +0900

    Title: 일 수 구하기, Time: 204ms, Memory: 10MB, Status: Passed - CodeTree

--- 240724/일 수 구하기/number-of-days-in-month.java
+++ 240724/일 수 구하기/number-of-days-in-month.java
@@ -8,14 +8,21 @@ public class Main {
 
         // 31일 : 1월, 3월, 5월, 7월, 8월, 10월, 12월
         // 30일 : 4월, 6월, 9월, 11월
-        // 28일 : 2월
-
-        if (n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12) {
-            System.out.println(31);
-        } else if (n == 2) {
+        // 28일 : 2월        
+        if (n == 2) {
             System.out.println(28);
+        } else if (n <= 7) {
+            if (n % 2 == 1) {
+                System.out.println(31);
+            } else {
+                System.out.println(30);
+            }
         } else {
-            System.out.println(30);
+            if (n % 2 == 1) {
+                System.out.println(30);
+            } else {
+                System.out.println(31);
+            }
         }
     }
 }
