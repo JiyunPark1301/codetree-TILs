5b7112c6a207e5de659cce95035c3ea907a5bcd9
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-28 16:47:48 +0900

    Title: 특정 조건의 온전수 구하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 240728/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         
         for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0 || i % 100 == 5 || (i % 3 == 0 && i % 9 != 0))
+            if (i % 2 == 0 || i % 10 == 5 || (i % 3 == 0 && i % 9 != 0))
                 continue;
             System.out.print(i + " ");
         }
