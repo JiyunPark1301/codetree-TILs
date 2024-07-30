0b119f05f9d36cf1b956646804953a89807bd5b1
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-30 13:05:38 +0900

    Title: 소수 판별기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240730/소수 판별기/prime-number-discriminator.java
+++ 240730/소수 판별기/prime-number-discriminator.java
@@ -6,7 +6,7 @@ public class Main {
 
         int n = sc.nextInt();
         boolean isSatisfied = true;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0)
                 isSatisfied = false;
         }
