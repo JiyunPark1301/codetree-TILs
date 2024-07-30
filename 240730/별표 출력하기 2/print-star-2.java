b49a9d7efef4b19b8a14d38c6e96d0862d9613e0
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-30 13:53:29 +0900

    Title: 별표 출력하기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/별표 출력하기 2/print-star-2.java
+++ 240730/별표 출력하기 2/print-star-2.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) { 
-            for (int j = n - i + 1; j >= 1; j--) {
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
             } 
             System.out.println();
