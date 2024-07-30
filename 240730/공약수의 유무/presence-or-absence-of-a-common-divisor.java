940f2e5bcaed347b61c4f9fc544efaac99e0009a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-07-30 12:57:08 +0900

    Title: 공약수의 유무, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240730/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -9,7 +9,7 @@ public class Main {
 
         boolean isSatisfied = false;
         for (int i = a; i <= b; i++) {
-            if (1920 % i == 0 || 2880 % i == 0) {
+            if (1920 % i == 0 && 2880 % i == 0) {
                 isSatisfied = true;
                 break;
             }
