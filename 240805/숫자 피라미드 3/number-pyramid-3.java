7f229e3b18e62a61e8cd7973d0ac658da282d28d
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-05 00:26:36 +0900

    Title: 숫자 피라미드 3, Time: 181ms, Memory: 11MB, Status: Passed - CodeTree

--- 240805/숫자 피라미드 3/number-pyramid-3.java
+++ 240805/숫자 피라미드 3/number-pyramid-3.java
@@ -5,12 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int cnt;
         for (int i = 1; i <= n; i++) {
-            cnt = i;
             for (int j = 1; j <= i; j++) {
-                System.out.print(cnt + " ");
-                cnt = i * (j + 1);
+                System.out.print(i * j + " ");
             }
             System.out.println();
         }
