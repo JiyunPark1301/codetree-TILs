998ff592f434a4c67241b50567924b0d804187b5
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-08 13:36:59 +0900

    Title: 가장 큰 수 고르기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/가장 큰 수 고르기/picking-biggest-number.java
+++ 240808/가장 큰 수 고르기/picking-biggest-number.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        final int MAX_VAL = Integer.MIN_VALUE;
-        int maxVal = MAX_VAL;
+        final int MIN_VAL = Integer.MIN_VALUE;
+        int maxVal = MIN_VAL;
         for (int i = 0; i < 10; i++) {
             int x = sc.nextInt();
             if (x > maxVal)
