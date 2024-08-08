7bd5a79424ce3910c06b63b94d8a2bd02e68c9e5
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-08 14:04:29 +0900

    Title: n개의 숫자 중 최대 2개, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240808/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 0; j < N - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.print(arr[N - 1] + " " + arr[N - 2]);
+    }
+}
