0c95970f6ad272059d48b325104cb9366b1d2e2b
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-08 15:18:11 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 200ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240808/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int idx = N - 1;
+
+        while (idx >= 0) {
+            int max = -1;
+            int endIdx = 0;
+            for (int i = 0; i <= idx; i++) {
+                if (max < arr[i]) {
+                    max = arr[i];
+                    endIdx = i;
+                }
+            }
+            idx = endIdx - 1;
+            System.out.print(endIdx + 1 + " ");
+        }
+    }
+}
