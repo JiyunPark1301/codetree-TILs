7fe8ff7cfedf1e9a479b03a3879bd8105194704e
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-09 13:47:25 +0900

    Title: 대문자로 바꾸기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240809/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] arr = new char[5][3];
+        //65~90(대문자),97~122(소문자)
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = sc.next().charAt(0);
+                System.out.print((char)(arr[i][j]-32) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
