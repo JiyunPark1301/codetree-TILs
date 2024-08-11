a904fa1271d83e8b985c83c8c44fa4b5a1512040
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-12 02:47:01 +0900

    Title: 함수를 이용한 온전수 판별, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240812/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = a; i <= b; i++) {
+            if (isOnjunSoo(i))
+                cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static boolean isOnjunSoo(int a) {
+        if (a % 2 == 0)
+            return false;
+        if (a % 10 == 5)
+            return false;
+        if (a % 3 == 0 && a % 9 != 0)
+            return false;
+        
+        return true;
+    }
+}
