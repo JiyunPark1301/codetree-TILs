abe1b9b317eced9f17ab1471da3d6f5f5c1ce18a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-11 17:17:48 +0900

    Title: 소문자와 숫자, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240811/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                System.out.print((char)(c - 'A' + 'a'));
+            } else if (c >= 'a' && c <= 'z') {
+                System.out.print(c);
+            } else if (Character.isDigit(c)) {
+                System.out.print(c);
+            }
+        }
+    }
+}
