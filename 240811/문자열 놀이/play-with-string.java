2b39eb7a749786b472ecd4007efe4937fa18ec77
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-11 15:11:36 +0900

    Title: 문자열 놀이, Time: 172ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240811/문자열 놀이/play-with-string.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int x = sc.nextInt();
+            if (x == 1) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                if (a < b) {
+                    s = s.substring(0,a - 1) + s.charAt(b - 1) + s.substring(a, b - 1) + s.charAt(a - 1) + s.substring(b);
+                } else {
+                    s = s.substring(0,b - 1) + s.charAt(a - 1) + s.substring(b, a - 1) + s.charAt(b - 1) + s.substring(a);
+                }
+                System.out.println(s);
+            } 
+            
+            if (x == 2) {
+                char a = sc.next().charAt(0);
+                char b = sc.next().charAt(0);
+                s = s.replace(a, b);
+                System.out.println(s);
+            }
+        }
+    }
+}
