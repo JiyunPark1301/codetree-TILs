4d63967a1a1c3d13b566828a9937012900a2a980
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-12 00:32:00 +0900

    Title: 숫자로 이루어진 사각형, Time: 214ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240812/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        printNumber(N);
+    }
+
+    public static void printNumber(int N) {
+        int num = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(num + " ");
+                
+                if (num == 9)
+                    num = 1;
+                else
+                    num++;
+            }
+            System.out.println();
+        }
+    }
+}
