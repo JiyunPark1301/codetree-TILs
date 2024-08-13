fef02f8b5c748331d0b674a0bec613110bc218a8
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-13 17:05:21 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

--- 240813/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 240813/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -14,11 +14,9 @@ public class Main {
     }
 
     public static boolean hasDifferentLetters(String str) {
-        for (int i = 0; i < str.length() - 1; i++) {
-            for (int j = i + 1; j < str.length(); j++) {
-                if (str.charAt(i) != str.charAt(j)) {
-                    return true;
-                }
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(0)) {
+                return true;
             }
         }
 
