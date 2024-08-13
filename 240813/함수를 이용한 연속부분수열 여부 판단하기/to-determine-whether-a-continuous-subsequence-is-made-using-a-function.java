c47840519fa63ed2b281cca931b5f3f5b98fe499
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-13 15:24:25 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240813/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240813/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static int[] arr1 = new int[MAX_N];
+    public static int[] arr2 = new int[MAX_N];
+
+    public static int n1;
+    public static int n2;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n1 = sc.nextInt(); // 수열A의 원소 개수
-        int n2 = sc.nextInt(); // 수열B의 원소 개수
+        n1 = sc.nextInt(); // 수열A의 원소 개수
+        n2 = sc.nextInt(); // 수열B의 원소 개수
         
-        int[] arr1 = new int[n1];
-        int[] arr2 = new int[n2];
 
         for (int i = 0; i < n1; i++) {
             arr1[i] = sc.nextInt();
@@ -18,28 +23,29 @@ public class Main {
             arr2[i] = sc.nextInt();
         }
 
-        if (isSubsequence(n1, n2, arr1, arr2)) {
+        if (isSubsequence()) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
     }
 
-    public static boolean isSubsequence(int n1, int n2, int[] arr1, int[] arr2) {
+    public static boolean isSubsequence() {
         for (int i = 0; i < n1 - n2 + 1; i++) {
-            boolean isSame = true;
-            for (int j = 0; j < n2; j++) {
-                if (arr1[i + j] != arr2[j]) {
-                    isSame = false;
-                    break;
-                }
-            }
-
-            if (isSame) {
+            if (isSame(i)) {
                 return true;
             }
         }
-
         return false;
+    }
+
+    public static boolean isSame(int i) {
+        for (int j = 0; j < n2; j++) {
+            if (arr1[i + j] != arr2[j]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
