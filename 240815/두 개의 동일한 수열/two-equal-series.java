396aa3cf2076f198c3a8cca78aa3780f840255b4
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-15 16:37:02 +0900

    Title: 두 개의 동일한 수열, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240815/두 개의 동일한 수열/two-equal-series.java
+++ 240815/두 개의 동일한 수열/two-equal-series.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 public class Main {
+    public static int n;
+    public static int[] arrA = new int[100];
+    public static int[] arrB = new int[100];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arrA = new int[n];
-        int[] arrB = new int[n];
-
+        n = sc.nextInt();
+        
         for (int i = 0; i < n; i++) {
             arrA[i] = sc.nextInt();
         }
@@ -16,20 +18,21 @@ public class Main {
             arrB[i] = sc.nextInt();
         }
 
-        Arrays.sort(arrA);
-        Arrays.sort(arrB);
+        Arrays.sort(arrA, 0, n);
+        Arrays.sort(arrB, 0, n);
 
-        boolean isSame = true;
-        for (int i = 0; i < n; i++) {
-            if (arrA[i] != arrB[i]) {
-                isSame = false;
-                break;
-            }
-        }
-
-        if (isSame)
+        if (isSame())
             System.out.println("Yes");
         else
             System.out.println("No");
+    }
+
+    public static boolean isSame() {
+        for (int i = 0; i < n; i++) {
+            if (arrA[i] != arrB[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
