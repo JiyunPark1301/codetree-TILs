9ad327332baf94eef987c0b614a260aa1dee6dc9
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-16 16:07:16 +0900

    Title: 비오는 날, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240816/비오는 날/rainy-day.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Weather {
+    String date;
+    String yoil;
+    String w;
+
+    public Weather(String date, String yoil, String w) {
+        this.date = date;
+        this.yoil = yoil;
+        this.w = w;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Weather[] arr = new Weather[n];
+
+        for (int i = 0; i < n; i++) {
+            String date = sc.next();
+            String yoil = sc.next();
+            String w = sc.next();
+
+            arr[i] = new Weather(date, yoil, w);
+        }
+
+        int lastIdx = 0;
+        boolean isInitialized = false;
+        for (int i = 0; i < n; i++) {
+            if (!isInitialized && "Rain".equals(arr[i].w)) {
+                isInitialized = true;
+                lastIdx = i;
+            }
+
+            if (!isInitialized)
+                continue;
+            
+            if (!"Rain".equals(arr[i].w))
+                continue;
+
+            if (arr[i].date.compareTo(arr[lastIdx].date) < 0) 
+                lastIdx = i;
+        }
+
+        System.out.print(arr[lastIdx].date + " " + arr[lastIdx].yoil + " " + arr[lastIdx].w);
+    }
+}
