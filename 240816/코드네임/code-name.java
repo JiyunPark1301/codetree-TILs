7eef0402382e698bef54e2d6341522f57fffd102
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-16 15:15:52 +0900

    Title: 코드네임, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240816/코드네임/code-name.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class NewClass {
+    char codeName;
+    int score;
+
+    public NewClass(char codeName, int score) {
+        this.codeName = codeName;
+        this.score = score;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        NewClass[] arr = new NewClass[5];
+
+        for (int i = 0; i < 5; i++) {
+            char codeName = sc.next().charAt(0);
+            int score = sc.nextInt();
+
+            arr[i] = new NewClass(codeName, score);
+        }
+
+        int min = arr[0].score;
+        int minIdx = 0;
+        for (int i = 1; i < 5; i++) {
+            if (arr[i].score < min) {
+                min = arr[i].score;
+                minIdx = i;
+            }
+        }
+
+        System.out.print(arr[minIdx].codeName + " " + arr[minIdx].score);
+    }
+}
