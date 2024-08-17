1adc8854bd8c0d7a73065ac354482085fe8a3a28
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-17 12:59:50 +0900

    Title: 그 요일은, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240817/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        String yoil = sc.next();
+
+        int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] yoilArr = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int x = d1;
+        for (int i = 1; i < m1; i++) {
+            x += days[i];
+        }
+
+        int y = d2;
+        for (int i = 1; i < m2; i++) {
+            y += days[i];
+        }
+
+        int idx = 0;
+        for (int i = 0; i < yoilArr.length; i++) {
+            if (yoil.equals(yoilArr[i])) {
+                idx = i;
+                break;
+            }
+        }
+        int diff = y - x;
+        int cnt = 0;
+        for (int i = 1; i <= diff; i++) {
+            if (7 * i - 2 <= diff)
+                cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
