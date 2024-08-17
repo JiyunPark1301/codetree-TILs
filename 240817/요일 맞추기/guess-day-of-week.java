3aaa2794bedbecd45878117c0b93a245847d3b12
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-17 12:17:21 +0900

    Title: 요일 맞추기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240817/요일 맞추기/guess-day-of-week.java
+++ 240817/요일 맞추기/guess-day-of-week.java
@@ -14,15 +14,15 @@ public class Main {
         int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         int x = d1;
-        for (int i = 1; i <= m1; i++) {
+        for (int i = 1; i < m1; i++) {
             x += days[i];
         }
 
         int y = d2;
-        for (int i = 1; i <= m2; i++) {
+        for (int i = 1; i < m2; i++) {
             y += days[i];
         }
-        int diff = y - x > 0 ? y - x - 1 : y - x;
+        int diff = y - x;
         while (diff < 0) {
             diff += 7;
         }
