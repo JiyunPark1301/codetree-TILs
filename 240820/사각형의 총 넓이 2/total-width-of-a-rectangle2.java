c81fffaeeaeb2e401abda011c5fbf48b765a06a2
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-20 21:26:40 +0900

    Title: 사각형의 총 넓이 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240820/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+        int[][] arr = new int[200][200];
+        for (int i = 0; i < N; i++) {
+            int x1 = sc.nextInt() + 100;
+            int y1 = sc.nextInt() + 100;
+            int x2 = sc.nextInt() + 100;
+            int y2 = sc.nextInt() + 100;
+
+            for (int j = x1; j <= x2 - 1; j++) {
+                for (int k = y1; k <= y2 - 1; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 200; i++) {
+            for (int j = 0; j < 200; j++) {
+                result += arr[i][j];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
