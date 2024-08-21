24c18d8319c966f8d7cdb754e5bb3681a4ab017a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-21 21:35:20 +0900

    Title: 연속되는 수 2, Time: 179ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240821/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = -1;
+        int cnt = 1;
+        for (int i = 1; i < N; i++) {   
+            if (arr[i] == arr[i - 1]) {
+                cnt++;
+            } else {
+                if (max < cnt) {
+                    max = cnt;
+                    cnt = 1;
+                }
+            }
+        }
+
+        if (max < cnt)
+            max = cnt;
+
+        System.out.print(max);
+    }
+}
