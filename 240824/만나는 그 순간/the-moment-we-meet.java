00a61d1c431bdacee6a3587d9455fc4caed50f18
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-24 17:12:06 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240824/만나는 그 순간/the-moment-we-meet.java
+++ 240824/만나는 그 순간/the-moment-we-meet.java
@@ -10,47 +10,47 @@ public class Main {
         int[] arr1 = new int[1001];
         int[] arr2 = new int[1001];
 
-        int second = 1;
+        int second1 = 1;
         int currPos = 0;
         for (int i = 0; i < N; i++) {
             char direction = sc.next().charAt(0);
             int x = sc.nextInt();
             
             if (direction == 'R') {
-                for (int j = second; j < second + x; j++) {
+                for (int j = second1; j < second1 + x; j++) {
                     arr1[j] = ++currPos;
                 }
             } else {
-                for (int j = second; j < second + x; j++) {
+                for (int j = second1; j < second1 + x; j++) {
                     arr1[j] = --currPos;
                 }
             }
 
-            second += x;
+            second1 += x;
         }
 
-        second = 1;
+        int second2 = 1;
         currPos = 0;
         for (int i = 0; i < M; i++) {
             char direction = sc.next().charAt(0);
             int x = sc.nextInt();
             
             if (direction == 'R') {
-                for (int j = second; j < second + x; j++) {
+                for (int j = second2; j < second2 + x; j++) {
                     arr2[j] = ++currPos;
                 }
             } else {
-                for (int j = second; j < second + x; j++) {
+                for (int j = second2; j < second2 + x; j++) {
                     arr2[j] = --currPos;
                 }
             }
 
-            second += x;
+            second2 += x;
         }
 
         // 두 배열을 비교하여 처음 만나는 순간 구하기
         int result = -1;
-        for (int i = 1; i < 1001; i++) {
+        for (int i = 1; i < Math.min(second1, second2); i++) {
             if (arr1[i] == arr2[i]) {
                 result = i;
                 break;
