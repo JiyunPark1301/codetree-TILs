5a6c2c59b847fb19978d86fdfc9f2b1faf9d27e4
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-25 23:17:08 +0900

    Title: 방향에 맞춰 이동, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240825/방향에 맞춰 이동/move-in-direction.java
+++ 240825/방향에 맞춰 이동/move-in-direction.java
@@ -10,22 +10,22 @@ public class Main {
 
         int x = 0, y = 0; //현재 좌표
         for (int i = 0; i < N; i++) {
-            char dir = sc.next().charAt(0); //방향
+            char cdir = sc.next().charAt(0); //방향
             int dis = sc.nextInt(); //움직인거리
 
-            if (dir == 'N') {//북
-                x += dx[3] * dis;
-                y += dy[3] * dis;
-            } else if (dir == 'E') {//동
-                x += dx[0] * dis;
-                y += dy[0] * dis;
-            } else if (dir == 'S') {//남
-                x += dx[1] * dis;
-                y += dy[1] * dis;
-            } else if (dir == 'W') {//서
-                x += dx[2] * dis;
-                y += dy[2] * dis;
+            int dir;
+            if (cdir == 'N') {//북
+                dir = 3;
+            } else if (cdir == 'E') {//동
+                dir = 0;
+            } else if (cdir == 'S') {//남
+                dir = 1;
+            } else {//서
+                dir = 2; 
             }
+
+            x += dx[dir] * dis;
+            y += dy[dir] * dis;
         }
 
         System.out.print(x + " " + y);
