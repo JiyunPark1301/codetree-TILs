05127f249a8a315c53f1488c3bc479e63eecbe22
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-28 14:40:51 +0900

    Title: 2진수로 변환하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[17];
+
+        int cnt = 0;
+        while(true) {
+            if (n < 2) {
+                arr[cnt] = n;
+                break;
+            }
+
+            arr[cnt++] = n % 2;
+            n /= 2; 
+        }
+
+        for (int i = cnt; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
