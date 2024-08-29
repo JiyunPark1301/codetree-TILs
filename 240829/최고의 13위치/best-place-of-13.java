41c5dbecb54835bbcf719198abc3b993cb67e3ef
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-29 12:43:47 +0900

    Title: 최고의 13위치, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - 2; j++) {
+                max = Math.max(max, arr[i][j] + arr[i][j + 1] + arr[i][j + 2]);
+            }
+        }
+
+        System.out.print(max);
+    }
+}
