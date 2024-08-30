6c94325fc13ef2a8bbbca6363f704e7c6a9f1f8a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-08-30 18:59:49 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240830/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+
+        char[][] arr = new char[R][C];
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                arr[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 1; i < C - 2; i++) {
+            for (int j = 1; j < R - 2; j++) {
+                for (int k = i + 1; k < C - 1; k++) {
+                    for (int r = j + 1; r < R - 1; r++) {
+                        if (arr[0][0] == 'W') {
+                            if (arr[i][j] == 'B' && arr[k][r] == 'W')
+                                cnt++;
+                        } else {
+                            if (arr[i][j] == 'W' && arr[k][r] == 'B')
+                                cnt++;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
