0033416fa26df617bfb4c945d56e1188c6f6cb04
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-09-04 01:17:58 +0900

    Title: 이상한 진수 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240904/이상한 진수 2/awkward-digits-2.java
+++ 240904/이상한 진수 2/awkward-digits-2.java
@@ -5,15 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String a = sc.next();
-        int[] arr = new int[1025];
+        int max = Integer.MIN_VALUE; //최대값
+
         for (int i = 0; i < a.length(); i++) {
             char c = a.charAt(i);
             c = (c == '0') ? '1' : '0';
-            arr[i] = Integer.parseInt(a.substring(0, i) + c + a.substring(i + 1));
+            int val = Integer.parseInt(a.substring(0, i) + c + a.substring(i + 1));
+            max = Math.max(val, max);
         }
-
-        Arrays.sort(arr, 0, a.length()); //오름차순 정렬
-        int max = arr[a.length() - 1]; //최댓값
 
         int result = 0;
         int idx = 0;
