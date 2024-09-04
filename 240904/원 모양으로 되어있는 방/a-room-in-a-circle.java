5de37ce56ea86906380e66f4f50751ee9738c5b1
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-09-04 09:38:17 +0900

    Title: 원 모양으로 되어있는 방, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240904/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int result = 0;
+            int idx = 0;
+            for (int j = i; j < i + N; j++) { //시작위치 
+                result += arr[j % 5] * idx++;
+            }
+
+            min = Math.min(result, min);
+        }
+        System.out.println(min);
+    }
+} 
+
+//0 ~ 4
+//1 ~ 5 (1, 2, 3, 4, 0) j % 5
+//2 ~ 6 (2, 3, 4, 0, 1) 
+
+// 2번방에서 시작
+// 0 1 2 3 4
+// 4 7 8 6 4
+//(1 + 1) % 5 = 2
+//(2 + 1) % 5 = 3
+//(3 + 1) % 5 = 4
+//(4 + 1) % 5 = 0
+//(0 + 1) % 5 = 1
+
+// 3번방에서 시작
+// 0 1 2 3 4
+// 4 7 8 6 4
+//(2 + 1) % 5 = 3
+//(3 + 1) % 5 = 4
+//(4 + 1) % 5 = 0
+//(0 + 1) % 5 = 1
+//(1 + 1) % 5 = 2
