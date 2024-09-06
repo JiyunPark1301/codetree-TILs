10f3b144528b83b36e4de049ac52b9b82943789e
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-09-06 10:18:11 +0900

    Title: 아름다운 수열 2, Time: 202ms, Memory: 10MB, Status: Passed - CodeTree

--- 240906/아름다운 수열 2/beautiful-sequence-2.java
+++ 240906/아름다운 수열 2/beautiful-sequence-2.java
@@ -9,6 +9,7 @@ public class Main {
 
         int[] arrA = new int[N];
         int[] arrB = new int[M];
+        int[] tmp = new int[M];
 
         for (int i = 0; i < N; i++) {
             arrA[i] = sc.nextInt();
@@ -19,15 +20,17 @@ public class Main {
         }   
 
         int result = 0;
-        int[] copiedArrB = Arrays.copyOf(arrB, M);
-        Arrays.sort(copiedArrB); //오름차순 정렬
+        Arrays.sort(arrB); //오름차순 정렬
 
         for (int i = 0; i <= N - M; i++) {
-            int[] copiedArrA = Arrays.copyOfRange(arrA, i, i + M);
-            Arrays.sort(copiedArrA); //오름차순 정렬
+            for (int j = 0; j < M; j++) {
+                tmp[j] = arrA[i + j];
+            }
+            Arrays.sort(tmp);
+
             int cnt = 0;
             for (int j = 0; j < M; j++) {
-                if (copiedArrA[j] == copiedArrB[j])
+                if (tmp[j] == arrB[j])
                     cnt++;
             }
             if (cnt == M)
