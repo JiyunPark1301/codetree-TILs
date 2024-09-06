a9dd4869069c9a68bbd6ba0920d42a9612f0ca7a
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-09-06 12:20:43 +0900

    Title: 밭의 높이를 고르게하기, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

--- 240906/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
+++ 240906/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -5,21 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt(); //밭의 개수
-        int H = sc.nextInt(); //최소 횟수
-        int T = sc.nextInt(); //밭의 높이
+        int H = sc.nextInt(); //밭의 높이
+        int T = sc.nextInt(); //최소 횟수
         int[] costArr = new int[N]; //H높이로 나오게끔 하는 최소 비용
 
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
-            costArr[i] = Math.abs(x - T);
+            costArr[i] = Math.abs(x - H);
         }
 
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i <= N - H; i++) {
+        for (int i = 0; i <= N - T; i++) {
             int sum = 0;
-            for (int j = i; j < i + H; j++) {
+            for (int j = i; j < i + T; j++) {
                 sum += costArr[j];
             }
+            
             min = Math.min(min, sum);
         }
 
