3ca1fa3c823a820861e195dbe6533f39a51adab6
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-09-09 09:52:37 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 216ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240909/두 가지로 열리는 자물쇠/a-two-way-lock.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arrA = new int[3];
+        int[] arrB = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arrA[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            arrB[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <= N; k++) {
+                    if ((Math.abs(arrA[0] -  i) <= 2 || Math.abs(arrA[0] - i) >= N - 2) && (Math.abs(arrA[1] -  j) <= 2 || Math.abs(arrA[1] - j) >= N - 2) &&
+                        (Math.abs(arrA[2] -  k) <= 2 || Math.abs(arrA[2] - k) >= N - 2))
+                            cnt++;
+                    
+                    else if ((Math.abs(arrB[0] -  i) <= 2 || Math.abs(arrB[0] - i) >= N - 2) && (Math.abs(arrB[1] -  j) <= 2 || Math.abs(arrB[1] - j) >= N - 2) &&
+                            (Math.abs(arrB[2] -  k) <= 2 || Math.abs(arrB[2] - k) >= N - 2))
+                            cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+} 
+
+// 1 2 3 4 5 6 7 8 9 10   1 2 3
+/**
+10
+1 2 3
+5 6 7
+
+9 - 1 = 8
+10 - 1 = 9
+
+*/
