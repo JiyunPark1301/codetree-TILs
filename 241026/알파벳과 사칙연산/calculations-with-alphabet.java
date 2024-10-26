f8fa2f3e9a0d5cb6c8c4fb89c635146480916ec6
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-10-26 16:54:44 +0900

    Title: 알파벳과 사칙연산, Time: 3051ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

---
+++ 241026/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    static String str;
+    static int[] alphabet = new int[6];
+    static int answer = Integer.MIN_VALUE;
+
+    public static int calculate(char c, int x, int y) {
+        if (c == '+')
+            return x + y;
+        if (c == '-')
+            return x - y;
+        
+        return x * y;
+    }
+
+    public static void dfs(int idx) {
+        if (idx >= str.length()) {
+            int val = 0;
+            char op = ' ';
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                //알파벳인 경우
+                if ('a' <= c && c <= 'z') {
+                    if (op == ' ') {
+                        val = alphabet[c - 'a'];
+                    } else {
+                        val = calculate(op, val, alphabet[c - 'a']);
+                    }
+                } else {
+                    op = c;
+                }
+            }
+
+            answer = Math.max(answer, val);
+            return;
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            alphabet[str.charAt(idx) - 'a'] = i;
+            dfs(idx + 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+        
+        dfs(0);
+
+        System.out.println(answer);
+    }
+}
