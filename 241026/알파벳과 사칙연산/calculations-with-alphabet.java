c2dd189a953b7d1fada944c7f1a05142c9ad5a54
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-10-26 17:26:35 +0900

    Title: 알파벳과 사칙연산, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241026/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 241026/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -6,48 +6,44 @@ public class Main {
     static int answer = Integer.MIN_VALUE;
 
     public static int calculate(char c, int x, int y) {
-        if (c == '+')
-            return x + y;
-        if (c == '-')
-            return x - y;
-        
-        return x * y;
+        if (c == '+') return x + y;
+        if (c == '-') return x - y;
+        return x * y; // '*' 연산
     }
 
     public static void dfs(int idx) {
-        if (idx == str.length() + 1) {
+        if (idx == str.length()) { // 종료 조건
             int val = 0;
             char op = ' ';
             for (int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
-                //알파벳인 경우
-                if ('a' <= c && c <= 'z') {
+                if ('a' <= c && c <= 'f') {
                     if (op == ' ') {
                         val = alphabet[c - 'a'];
                     } else {
                         val = calculate(op, val, alphabet[c - 'a']);
                     }
                 } else {
-                    op = c;
+                    op = c; // 연산자 업데이트
                 }
             }
-
             answer = Math.max(answer, val);
             return;
         }
 
-        for (int i = 1; i <= 4; i++) {
-            alphabet[str.charAt(idx) - 'a'] = i;
-            dfs(idx + 2);
+        // 인덱스가 알파벳의 인덱스인 경우만 처리
+        if ('a' <= str.charAt(idx) && str.charAt(idx) <= 'f') {
+            for (int i = 1; i <= 4; i++) {
+                alphabet[str.charAt(idx) - 'a'] = i;
+                dfs(idx + 1); // 다음 인덱스로 진행
+            }
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         str = sc.next();
-        
         dfs(0);
-
         System.out.println(answer);
     }
 }
