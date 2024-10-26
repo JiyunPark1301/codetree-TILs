62df7bf15bd1357b4ae27632ba792b830f9bdaf3
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-10-26 17:30:35 +0900

    Title: 알파벳과 사칙연산, Time: 3008ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

--- 241026/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 241026/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -1,22 +1,33 @@
 import java.util.*;
 
 public class Main {
+    //주어진 문자열
     static String str;
+    
+    //알파벳에 해당하는 숫자를 담기 위한 배열
+    //'a' ~ 'f'-> 0 ~ 5번 인덱스
     static int[] alphabet = new int[6];
+
+    //결과값
     static int answer = Integer.MIN_VALUE;
 
     public static int calculate(char c, int x, int y) {
-        if (c == '+') return x + y;
-        if (c == '-') return x - y;
-        return x * y; // '*' 연산
+        if (c == '+')
+            return x + y;
+        if (c == '-')
+            return x - y;
+        
+        return x * y;
     }
 
     public static void dfs(int idx) {
-        if (idx == str.length()) { // 종료 조건
+        if (idx > str.length()) {
             int val = 0;
             char op = ' ';
+            //알파벳에 숫자를 대입 후 연산
             for (int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
+                //알파벳인 경우
                 if ('a' <= c && c <= 'f') {
                     if (op == ' ') {
                         val = alphabet[c - 'a'];
@@ -24,26 +35,31 @@ public class Main {
                         val = calculate(op, val, alphabet[c - 'a']);
                     }
                 } else {
-                    op = c; // 연산자 업데이트
+                    //연산자인 경우
+                    op = c;
                 }
             }
+
+            //연산결과의 최댓값을 구한다.
             answer = Math.max(answer, val);
             return;
         }
 
-        // 인덱스가 알파벳의 인덱스인 경우만 처리
-        if ('a' <= str.charAt(idx) && str.charAt(idx) <= 'f') {
-            for (int i = 1; i <= 4; i++) {
-                alphabet[str.charAt(idx) - 'a'] = i;
-                dfs(idx + 1); // 다음 인덱스로 진행
-            }
+        //1~4까지의 숫자를 문자에 담는다.
+        for (int i = 1; i <= 4; i++) {
+            alphabet[str.charAt(idx) - 'a'] = i;
+
+            //연산자는 건너뛰어야 하므로 현재 인덱스에 +2를 해준다.
+            dfs(idx + 2);
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         str = sc.next();
+        
         dfs(0);
+
         System.out.println(answer);
     }
 }
