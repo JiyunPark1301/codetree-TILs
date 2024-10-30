9775c36c3adf1684432ed538db3bfac06b9ecd47
Jiyun Park <77823753+JiyunPark1301@users.noreply.github.com>
2024-10-30 11:33:30 +0900

    Title: 최소 점프 횟수, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/최소 점프 횟수/min-num-of-jumps.java
+++ 241030/최소 점프 횟수/min-num-of-jumps.java
@@ -1,28 +1,25 @@
 import java.util.*;
 
 public class Main {
+    static final int MAX = Integer.MAX_VALUE;
     static int n;
     static int[] arr;
-    static int answer = Integer.MAX_VALUE;
+    static int answer = MAX;
+
+    public static boolean inRange(int idx) {
+        return idx < n;
+    }
 
     public static void dfs(int idx, int jumpCnt) {
-        if (arr[idx] == 0) {
-            answer = -1;
-            return;
-        }
-
-        if (jumpCnt >= n) {
-            answer = -1;
-            return;
-        }
-
-        if (idx >= n - 1) {
+        if (idx == n - 1) {
             answer = Math.min(answer, jumpCnt);
             return;
         }
 
-        for (int i = 0; i <= arr[idx]; i++) {
-            dfs(idx + i, jumpCnt + 1);
+        for (int i = 1; i <= arr[idx]; i++) {
+            if (inRange(idx + i)) {
+                dfs(idx + i, jumpCnt + 1);
+            }
         }
     }
 
@@ -37,6 +34,9 @@ public class Main {
 
         dfs(0, 0);
 
-        System.out.println(answer);
+        if (answer == MAX)
+            System.out.println(-1);
+        else
+            System.out.println(answer);
     }
 }
